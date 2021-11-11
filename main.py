ab55448d1af8448d70a0563caad0ba5ab6509eb1
Jori <jvalkenhoff@outlook.com>
2021-11-12 00:32:30 +0100

    recaptcha issue on website, figuring out how to bypass...

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+# Importing modules
 from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+def get_chrome_driver():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
 
-driver.get('https://www.funda.nl/')
+    serv = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=serv, options=chrome_options)
+
+chrome_driver = get_chrome_driver()
+chrome_driver.get("https://www.funda.nl/koop/amsterdam/")
+# driver.get('https://www.funda.nl/')
