057b6e99ac84a2ba154a27dbc0409c40a07abbd1
Jori <jvalkenhoff@outlook.com>
2021-11-12 11:14:23 +0100

    bypass recaptcha with user-agent

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def get_chrome_driver():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
 
+    # Use this to bypass recaptcha
+    chrome_options.add_argument('--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"')
+
     serv = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=serv, options=chrome_options)
 
